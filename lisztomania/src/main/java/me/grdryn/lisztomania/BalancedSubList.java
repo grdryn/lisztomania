6011037f6ec55d3e58af89eaa11d041f4b5a0b44
Gerard Ryan <gerard@ryan.lt>
2014-09-27 18:17:04 +0100

    Flesh out BalancedSubList implementation

--- lisztomania/src/main/java/me/grdryn/lisztomania/BalancedSubList.java
+++ lisztomania/src/main/java/me/grdryn/lisztomania/BalancedSubList.java
@@ -7,119 +7,103 @@ import java.util.ListIterator;
 
 public class BalancedSubList<E> implements List<E> {
 
-    public boolean add(final E arg0) {
-        // TODO Auto-generated method stub
-        return false;
+    private final List<E> backingList;
+
+    public BalancedSubList(final List<E> originalList, final int percentage) {
+        this.backingList = originalList;
     }
 
-    public void add(final int arg0, final E arg1) {
-        // TODO Auto-generated method stub
+    public boolean add(final E arg0) {
+        return backingList.add(arg0);
+    }
 
+    public void add(final int index, final E element) {
+        backingList.add(index, element);
     }
 
     public boolean addAll(final Collection<? extends E> arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.addAll(arg0);
     }
 
     public boolean addAll(final int arg0, final Collection<? extends E> arg1) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.addAll(arg0, arg1);
     }
 
     public void clear() {
-        // TODO Auto-generated method stub
+        backingList.clear();
 
     }
 
     public boolean contains(final Object arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.contains(arg0);
     }
 
     public boolean containsAll(final Collection<?> arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.containsAll(arg0);
     }
 
     public E get(final int arg0) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.get(arg0);
     }
 
     public int indexOf(final Object arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+        return backingList.indexOf(arg0);
     }
 
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.isEmpty();
     }
 
     public Iterator<E> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.iterator();
     }
 
     public int lastIndexOf(final Object arg0) {
-        // TODO Auto-generated method stub
-        return 0;
+        return backingList.lastIndexOf(arg0);
     }
 
     public ListIterator<E> listIterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.listIterator();
     }
 
     public ListIterator<E> listIterator(final int arg0) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.listIterator(arg0);
     }
 
     public boolean remove(final Object arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.remove(arg0);
     }
 
     public E remove(final int arg0) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.remove(arg0);
     }
 
     public boolean removeAll(final Collection<?> arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.removeAll(arg0);
     }
 
     public boolean retainAll(final Collection<?> arg0) {
-        // TODO Auto-generated method stub
-        return false;
+        return backingList.retainAll(arg0);
     }
 
     public E set(final int arg0, final E arg1) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.set(arg0, arg1);
     }
 
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return backingList.size();
     }
 
     public List<E> subList(final int arg0, final int arg1) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.subList(arg0, arg1);
     }
 
     public Object[] toArray() {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.toArray();
     }
 
     public <T> T[] toArray(final T[] arg0) {
-        // TODO Auto-generated method stub
-        return null;
+        return backingList.toArray(arg0);
     }
 
 }
