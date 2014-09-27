8f3bc166462afe4d954787050412daad31431239
Gerard Ryan <gerard@ryan.lt>
2014-09-27 18:56:34 +0100

    Add initial test (failing)

---
+++ lisztomania/src/test/java/me/grdryn/lisztomania/BalancedSubListTest.java
@@ -0,0 +1,14 @@
+package me.grdryn.lisztomania;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BalancedSubListTest {
+
+    @Test
+    public void testBalancedSubList() {
+        fail("Not yet implemented"); // TODO
+    }
+
+}
