83ea6fadf4f100b5a8c8cf602dc2737b2c57e9d6
Gerard Ryan <gerard@ryan.lt>
2014-09-27 23:57:28 +0100

    Prove sublist 1 when list 1

--- lisztomania/src/test/java/me/grdryn/lisztomania/BalancedSubListTest.java
+++ lisztomania/src/test/java/me/grdryn/lisztomania/BalancedSubListTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.assertFalse;
 import java.util.Arrays;
 import java.util.List;
 
+import net.java.quickcheck.Generator;
+
 import org.junit.Test;
 
 public class BalancedSubListTest {
@@ -41,6 +43,22 @@ public class BalancedSubListTest {
                     "Failed with original %s and sublist %s", originalList,
                     listUnderTest),
                     Math.round(originalList.size() / 2.0), listUnderTest.size());
+        }
+    }
+
+    @Test
+    public void testListSizeOne() {
+        final Generator<Integer> integerGenerator = integers(1, 100);
+        for (final List<Integer> originalList : someLists(integers(), 1, 1)) {
+            final int percentage = integerGenerator.next();
+
+            System.out.print(percentage + " : " + originalList);
+
+            final BalancedSubList<Integer> listUnderTest = new BalancedSubList<>(
+                    originalList, percentage);
+
+            assertEquals("Sublist should be 1 when list size is 1", 1,
+                    listUnderTest.size());
         }
     }
 
