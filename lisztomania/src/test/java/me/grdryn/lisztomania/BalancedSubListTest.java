5916e9c03a17ccd6eb966ecb7df121c73e5e5830
Gerard Ryan <gerard@ryan.lt>
2014-09-27 19:11:43 +0100

    Prove 100% sublist equals original List

--- lisztomania/src/test/java/me/grdryn/lisztomania/BalancedSubListTest.java
+++ lisztomania/src/test/java/me/grdryn/lisztomania/BalancedSubListTest.java
@@ -1,14 +1,24 @@
 package me.grdryn.lisztomania;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class BalancedSubListTest {
 
     @Test
-    public void testBalancedSubList() {
-        fail("Not yet implemented"); // TODO
+    public void testHundredPercent() {
+        final int percent = 100;
+        final List<Integer> originalList = Arrays.asList(0, 1, 2, 3, 4, 5, 6,
+                7, 8, 9);
+
+        final BalancedSubList<Integer> listUnderTest = new BalancedSubList<Integer>(
+                originalList, percent);
+
+        assertEquals(originalList, listUnderTest);
     }
 
 }
