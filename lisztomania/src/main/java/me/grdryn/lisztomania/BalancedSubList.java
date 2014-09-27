b475f7a1ebf0a4455caa0eec6e060ee929da6eb6
Gerard Ryan <gerard@ryan.lt>
2014-09-27 17:29:05 +0100

    Add initial empty BalancedSubList

---
+++ lisztomania/src/main/java/me/grdryn/lisztomania/BalancedSubList.java
@@ -0,0 +1,125 @@
+package me.grdryn.lisztomania;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class BalancedSubList<E> implements List<E> {
+
+    public boolean add(final E arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void add(final int arg0, final E arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean addAll(final Collection<? extends E> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean addAll(final int arg0, final Collection<? extends E> arg1) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean contains(final Object arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean containsAll(final Collection<?> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public E get(final int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int indexOf(final Object arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int lastIndexOf(final Object arg0) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public ListIterator<E> listIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ListIterator<E> listIterator(final int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean remove(final Object arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public E remove(final int arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean removeAll(final Collection<?> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean retainAll(final Collection<?> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public E set(final int arg0, final E arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public List<E> subList(final int arg0, final int arg1) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public <T> T[] toArray(final T[] arg0) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
