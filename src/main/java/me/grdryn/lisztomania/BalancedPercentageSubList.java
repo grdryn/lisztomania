d1280c30d1385460569a32c90e1b58902a3ae663
Gerard Ryan <gerard@ryan.lt>
2014-10-01 20:52:52 +0100

    Add some Asciidoctor'ed javadocs

--- src/main/java/me/grdryn/lisztomania/BalancedPercentageSubList.java
+++ src/main/java/me/grdryn/lisztomania/BalancedPercentageSubList.java
@@ -19,12 +19,105 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * = BalancedPercentageSubList
+ *
+ * Use this class to get a {@link List} of elements picked from an existing
+ * {@link List}. The chosen elements are evenly spread across `originalList`,
+ * with the number of chosen elements decided by the `percentage` parameter to
+ * the constructor.
+ *
+ * For example, using the
+ * {@link BalancedPercentageSubList#BalancedPercentageSubList(List, double)
+ * first constructor} to get 50% of a {@link List} of 10 Integers would choose
+ * every second element from the original List:
+ *
+ * [source,java]
+ * --
+ *
+ * import java.util.List;
+ * import me.grdryn.lisztomania.BalancedPercentageSubList;
+ *
+ * class MyClass {
+ *
+ *     public static void main(String[] args) {
+ *         List<Integer> tenInts = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+ *         List<Integer> fiveInts = new BalancedPercentageSubList<>(tenInts, 50);
+ *
+ *         assert Arrays.asList(0, 2, 4, 6, 8) == fiveInts;
+ *     }
+ * }
+ *
+ * --
+ *
+ * == Specifying a shift/offset
+ *
+ * Alternatively, the
+ * {@link BalancedPercentageSubList#BalancedPercentageSubList(List, double, int)
+ * second constructor} can be used, where the third parameter `shift`, can be
+ * specified. This parameter is used to get a different `percentage` of the
+ * elements of the `originalList`.
+ *
+ * Adding a `shift` of 1 to the above example, would yield a List of the same
+ * length, but would start at the second element:
+ *
+ * [source,java]
+ * --
+ *
+ * import java.util.List;
+ * import me.grdryn.lisztomania.BalancedPercentageSubList;
+ *
+ * class MyClass {
+ *
+ *     public static void main(String[] args) {
+ *         List<Integer> tenInts = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+ *         List<Integer> fiveInts = new BalancedPercentageSubList<>(tenInts, 50, 1);
+ *
+ *         assert Arrays.asList(1, 3, 5, 7, 9) == fiveInts;
+ *     }
+ * }
+ *
+ * --
+ *
+ * @author Gerard Ryan
+ */
 public class BalancedPercentageSubList<E> extends AbstractPercentageSubList<E> {
 
+    /**
+     * Creates an instance from a `percentage` of the `originalList`.
+     *
+     * In the context of the
+     * {@link BalancedPercentageSubList#BalancedPercentageSubList(List, double, int)
+     * second constructor}, the `shift` is 0 here.
+     *
+     * @param originalList
+     *            The {@link List} to choose elements from
+     * @param percentage
+     *            The percentage of elements from `originalList` to choose
+     */
     public BalancedPercentageSubList(final List<E> originalList, final double percentage) {
         this(originalList, percentage, 0);
     }
 
+    /**
+     * Creates an instance from a `percentage` of `originalList` offset by
+     * `shift`.
+     *
+     * The `shift` parameter is used to decide what element of `originalList` to
+     * start picking from. This allows to get an offset sublist of elements.
+     *
+     * If, based on the `shift` parameter, and the percentage of elements to
+     * choose, the end of the List is reached, it will wrap around to the
+     * beginning again.
+     *
+     * @param originalList
+     *            The {@link List} to choose elements from
+     * @param percentage
+     *            The percentage of elements from `originalList` to choose
+     * @param shift
+     *            What position of `originalList` to start choosing elements
+     *            from
+     */
     public BalancedPercentageSubList(final List<E> originalList, final double percentage,
             final int shift) {
 
